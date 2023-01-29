3c20e2728efb0b399ea1f12d5591909b8dcbf74b
rahmadanu <sadewarahmadanu@gmail.com>
2023-01-29 21:52:15 +0700

    Set up navigation component

---
+++ app/src/main/java/com/dandev/storyapp/ui/host/HostActivity.kt
@@ -0,0 +1,34 @@
+package com.dandev.storyapp.ui.host
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.dandev.storyapp.R
+import com.dandev.storyapp.databinding.ActivityHostBinding
+
+class HostActivity : AppCompatActivity() {
+
+    private var _binding: ActivityHostBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpNavController()
+    }
+
+    private fun setUpNavController() {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostContainer.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
