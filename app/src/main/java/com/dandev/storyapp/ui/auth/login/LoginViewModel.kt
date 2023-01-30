64fd8114e0e28bdb79378da20fa60b17f6b054f6
rahmadanu <sadewarahmadanu@gmail.com>
2023-01-31 00:29:22 +0700

    Set up viewModels for authentication

---
+++ app/src/main/java/com/dandev/storyapp/ui/auth/login/LoginViewModel.kt
@@ -0,0 +1,31 @@
+package com.dandev.storyapp.ui.auth.login
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dandev.storyapp.data.remote.model.auth.LoginRequest
+import com.dandev.storyapp.data.remote.model.auth.LoginResponse
+import com.dandev.storyapp.domain.LoginUserUseCase
+import com.dandev.storyapp.util.wrapper.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(
+    private val loginUserUseCase: LoginUserUseCase
+): ViewModel() {
+    private val _loginResponse = MutableLiveData<Resource<LoginResponse>>()
+    val loginResponse: LiveData<Resource<LoginResponse>> = _loginResponse
+
+    fun loginUser(loginRequest: LoginRequest) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = loginUserUseCase(loginRequest)
+            viewModelScope.launch(Dispatchers.Main) {
+                _loginResponse.postValue(response)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/dandev/storyapp/ui/auth/register/RegisterViewModel.kt
@@ -0,0 +1,31 @@
+package com.dandev.storyapp.ui.auth.register
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dandev.storyapp.data.remote.model.auth.RegisterRequest
+import com.dandev.storyapp.data.remote.model.auth.RegisterResponse
+import com.dandev.storyapp.domain.RegisterUserUseCase
+import com.dandev.storyapp.util.wrapper.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class RegisterViewModel @Inject constructor(
+    private val registerUserUseCase: RegisterUserUseCase
+): ViewModel() {
+    private val _registerResponse = MutableLiveData<Resource<RegisterResponse>>()
+    val registerResponse: LiveData<Resource<RegisterResponse>> get() = _registerResponse
+
+    fun registerUser(registerRequest: RegisterRequest) {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = registerUserUseCase(registerRequest)
+            viewModelScope.launch(Dispatchers.Main) {
+                _registerResponse.postValue(response)
+            }
+        }
+    }
+}
