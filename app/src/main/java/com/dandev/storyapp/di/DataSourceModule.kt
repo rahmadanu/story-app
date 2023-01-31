2efe4b23ae5a3b0d18b22662bac71d62f3f355ee
rahmadanu <sadewarahmadanu@gmail.com>
2023-01-31 15:10:28 +0700

    Provide auth local data source with dependency injection

--- app/src/main/java/com/dandev/storyapp/di/DataSourceModule.kt
+++ app/src/main/java/com/dandev/storyapp/di/DataSourceModule.kt
@@ -1,5 +1,7 @@
 package com.dandev.storyapp.di
 
+import com.dandev.storyapp.data.local.data_source.AuthLocalDataSource
+import com.dandev.storyapp.data.local.data_source.AuthLocalDataSourceImpl
 import com.dandev.storyapp.data.remote.data_source.AuthRemoteDataSource
 import com.dandev.storyapp.data.remote.data_source.AuthRemoteDataSourceImpl
 import com.dandev.storyapp.data.remote.data_source.StoryRemoteDataSource
@@ -15,6 +17,9 @@ abstract class DataSourceModule {
 
     @Binds
     abstract fun provideAuthRemoteDataSource(authRemoteDataSourceImpl: AuthRemoteDataSourceImpl): AuthRemoteDataSource
+
+    @Binds
+    abstract fun provideAuthLocalDataSource(authLocalDataSourceImpl: AuthLocalDataSourceImpl): AuthLocalDataSource
 
     @Binds
     abstract fun provideStoryRemoteDataSource(storyRemoteDataSourceImpl: StoryRemoteDataSourceImpl): StoryRemoteDataSource
