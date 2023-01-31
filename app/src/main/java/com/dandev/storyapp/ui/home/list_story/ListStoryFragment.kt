67af30dad039df348d1e5c5181303183c406f9e3
rahmadanu <sadewarahmadanu@gmail.com>
2023-02-01 06:37:14 +0700

    Set up recyclerView adapter

--- app/src/main/java/com/dandev/storyapp/ui/home/list_story/ListStoryFragment.kt
+++ app/src/main/java/com/dandev/storyapp/ui/home/list_story/ListStoryFragment.kt
@@ -5,15 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dandev.storyapp.R
+import com.dandev.storyapp.databinding.FragmentListStoryBinding
+import com.dandev.storyapp.ui.home.list_story.adapter.ListStoryAdapter
 
 class ListStoryFragment : Fragment() {
+
+    private var _binding: FragmentListStoryBinding? = null
+    private val binding get() = _binding!!
+
+    private val adapter: ListStoryAdapter by lazy {
+        ListStoryAdapter{
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_story, container, false)
+        _binding = FragmentListStoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initList()
+    }
+
+    private fun initList() {
+        binding.rvListStory.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = this@ListStoryFragment.adapter
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/dandev/storyapp/ui/home/list_story/adapter/ListStoryAdapter.kt
@@ -0,0 +1,55 @@
+package com.dandev.storyapp.ui.home.list_story.adapter;
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.dandev.storyapp.data.remote.model.story.Story
+import com.dandev.storyapp.databinding.ItemStoryBinding
+
+class ListStoryAdapter(private val itemClick: (Story) -> Unit) :
+    RecyclerView.Adapter<ListStoryAdapter.ListStoryViewHolder>() {
+    
+    private val diffCallback = object : DiffUtil.ItemCallback<Story>() {
+        override fun areItemsTheSame(oldItem: Story, newItem: Story): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Story, newItem: Story): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, diffCallback)
+
+    fun submitList(list: List<Story?>?) {
+        differ.submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListStoryViewHolder {
+        val binding = ItemStoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ListStoryViewHolder(binding, itemClick)
+    }
+
+    override fun onBindViewHolder(holder: ListStoryViewHolder, position: Int) {
+        holder.bindView(differ.currentList[position])
+    }
+
+    override fun getItemCount(): Int = differ.currentList.size
+
+    class ListStoryViewHolder(
+        private val binding: ItemStoryBinding,
+        val itemClick: (Story) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bindView(item: Story) {
+            with(item) {
+                itemView.setOnClickListener { itemClick(this) }
+            }
+
+        }
+    }
+
+}
