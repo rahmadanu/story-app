4b62a90261bd38e0701cb971bfcce467dfadf1f6
rahmadanu <sadewarahmadanu@gmail.com>
2023-02-04 16:50:16 +0700

    Implement camera intent to take photos

--- app/src/main/java/com/dandev/storyapp/ui/home/add_story/AddStoryFragment.kt
+++ app/src/main/java/com/dandev/storyapp/ui/home/add_story/AddStoryFragment.kt
@@ -1,17 +1,41 @@
 package com.dandev.storyapp.ui.home.add_story
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.dandev.storyapp.R
 import com.dandev.storyapp.databinding.FragmentAddStoryBinding
+import com.dandev.storyapp.util.image.createCustomTempFile
+import java.io.File
 
 class AddStoryFragment : Fragment() {
 
     private var _binding: FragmentAddStoryBinding? = null
     private val binding get() = _binding!!
+
+    private var getFile: File? = null
+    private lateinit var currentPhotoPath: String
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+
+            val result = BitmapFactory.decodeFile(getFile?.path)
+            binding.ivAddImagePlaceholder.setImageBitmap(result)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +49,31 @@ class AddStoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setOnClickListener()
+    }
+
+    private fun setOnClickListener() {
+        binding.apply {
+            btnAddCamera.setOnClickListener {
+                startCamera()
+            }
+        }
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(requireActivity().packageManager)
+
+        createCustomTempFile(requireActivity().application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                requireActivity(),
+                getString(R.string.package_authority),
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 
     override fun onDestroyView() {
